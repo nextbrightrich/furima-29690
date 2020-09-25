a113506077d24a13250b0c86a2ec226719379b1f
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-09-25 18:19:15 +0900

    ログインしているユーザーだけが、出品ページへ遷移
    ローカルホストが調子悪く、
    ログインできるかはまだできていない。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '5555'
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 end
