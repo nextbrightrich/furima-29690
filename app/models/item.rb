424f0eba98e6c0abcbb95812149ae69f6a6a9086
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-09-25 21:10:14 +0900

    アクティブハッシュの空の保存とジャンル空の保存のバリデーションっぽい物
    元のカリキュラムからひっぱりなので途中

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,10 @@ class Item < ApplicationRecord
   belongs_to_active_hash :burden
   belongs_to_active_hash :area
   belongs_to_active_hash :days
+
+    #空の投稿を保存できないようにする　
+    validates :title, :text, presence: true
+
+    #ジャンルの選択が「--」の時は保存できないようにする一旦categoのみ
+    validates :category＿id , numericality: { other_than: 1 } 
 end
