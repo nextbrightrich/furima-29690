8e2bab185d16a82253187347ca99e58d425f1a41
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-09-25 20:47:37 +0900

    itemsモデルにアクティブハッシュのアソシエーション　５つ

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :burden
+  belongs_to_active_hash :area
+  belongs_to_active_hash :days
 end
