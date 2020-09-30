c4d66af4cb3d239ab940b19d12e6ec0bb7f314dd
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-09-30 14:31:10 +0900

    user バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :title, :text, presence: true
+  validates :nickname, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください' 
+
+  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ ,message: '名字を全角（漢字・ひらがな・カタカナ）で入力下さい'}
+  validates :familyname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ ,message: '名前を全角（漢字・ひらがな・カタカナ）で入力下さい'}
+  validates :firstnamekana, presence: true, format: { with: /\A[ァ-ン]+\z/ ,message: '名字を全角（カタカナ）で入力下さい'} 
+  validates :familynamekana, presence: true, format: { with: /\A[ァ-ン]+\z/ ,message: '名前を全角（カタカナ）で入力下さい'}
+  validates :birthday, presence: true
+
 end
