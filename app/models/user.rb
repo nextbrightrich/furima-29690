a1e53b2213d0e830d1e814eb1b04bc1f6cac392a
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-10-01 16:12:13 +0900

    バリデーション訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,12 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください' 
 
-  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ ,message: '名字を全角（漢字・ひらがな・カタカナ）で入力下さい'}
-  validates :familyname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ ,message: '名前を全角（漢字・ひらがな・カタカナ）で入力下さい'}
-  validates :firstnamekana, presence: true, format: { with: /\A[ァ-ン]+\z/ ,message: '名字を全角（カタカナ）で入力下さい'} 
-  validates :familynamekana, presence: true, format: { with: /\A[ァ-ン]+\z/ ,message: '名前を全角（カタカナ）で入力下さい'}
-  validates :birthday, presence: true
+
+with_options presence: true do
+  validates :firstname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+  validates :familyname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+  validates :firstnamekana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :familynamekana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :birthday, presence: format: { with: /\A[0-9]+\z/i, message: "is invalid. Input half-width characters."}
 
 end
