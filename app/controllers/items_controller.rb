c2c1d6b23142cb72114b06068cb95c683c9a859a
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-10-02 18:49:37 +0900

    アクティブハッシュ設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
-  def index
+  def index 
+    @items = Item.includes(:user).order("created_at DESC")
+  end
+ 
+  def new
+    @Item = Item.new(item_params)
+    if @item.save
+       redirect_to root_path
+    else
+      render :new
+    end
+  end
+  def item_params
+     params.require(:item).permit(:name, :price, :description, :image, :category_id, :status_id, :burden_id, :area_id, :day_id)
   end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,23 @@
+class Item < ApplicationRecord
+
+  belongs_to :user
+  has_one :customer
+  has_one_attached :image
+
+ extend ActiveHash::Associations::ActiveRecordExtensions
+ belongs_to_active_hash :category
+ belongs_to_active_hash :status
+ belongs_to_active_hash :burden
+ belongs_to_active_hash :area
+ belongs_to_active_hash :day
+
+ #商品名と価格が空の投稿を保存できないようにする 
+ validates :name, :price, :description,  :price, :category, :status,  :burden, :burden, :area, :day, presence: true
+
+ #ジャンルの選択が「--」の時は保存できないようにする一旦categoのみ
+ validates :category＿id , numericality: { other_than: 1 } 
+ validates :status_id , numericality: { other_than: 1 } 
+ validates :burden_id , numericality: { other_than: 1 } 
+ validates :area_id , numericality: { other_than: 1 } 
+ validates :day_id , numericality: { other_than: 1 } 
+end
