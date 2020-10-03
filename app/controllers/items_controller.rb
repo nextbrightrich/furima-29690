2bd2bc42cef641de50bdbcf5f2c181af81c4deba
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-10-03 16:08:12 +0900

    ログインしているユーザーだけが、出品ページへ遷移できること

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!,only:[:new , :create]
+  
   def index 
     @items = Item.includes(:user).order("created_at DESC")
   end
  
   def new
-    @Item = Item.new(item_params)
+    @item = Item.new
+  end
+
+  def create   
+    @item = Item.new(item_params)
     if @item.save
-       redirect_to root_path
+       redirect_to item_path(@item)
     else
       render :new
     end
+
   end
+
+  private
   def item_params
-     params.require(:item).permit(:name, :price, :description, :image, :category_id, :status_id, :burden_id, :area_id, :day_id)
+     params.require(:item).permit(:name, :price, :description, :image, :category_id, :status_id, :burden_id, :area_id, :day_id) .merge(user_id: current_user.id)
   end
 end
