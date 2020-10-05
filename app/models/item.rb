07c1e22c5311ed2c0c9a17ecfdba410ac4758dd5
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-10-05 22:47:56 +0900

    誤字の訂正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
   # 空の投稿を保存できないようにする
   validates :name, :price, :description, :image, :price, :category, :status, :burden, :burden, :area, :day, presence: true
 
-  # ジャンルの選択が「--」の時は保存できないようにする一旦categoのみ
+  # ジャンルの選択が「--」の時は保存できないようにする
   with_options presence: true do
   validates :category_id, numericality: { other_than: 1 }
   validates :status_id, numericality: { other_than: 1 }
