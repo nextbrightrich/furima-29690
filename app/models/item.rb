8b63bea6cf6d8d09c2a7cd060e4e0bb9f36d4605
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-10-06 10:48:39 +0900

    バリデーションまとめる

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,15 +14,15 @@ class Item < ApplicationRecord
   validates :name, :price, :description, :image, :price, :category, :status, :burden, :burden, :area, :day, presence: true
 
   # ジャンルの選択が「--」の時は保存できないようにする
-  with_options presence: true do
-  validates :category_id, numericality: { other_than: 1 }
-  validates :status_id, numericality: { other_than: 1 }
-  validates :burden_id, numericality: { other_than: 1 }
-  validates :area_id, numericality: { other_than: 1 }
-  validates :day_id, numericality: { other_than: 1 }
+  with_options numericality: {other_than: 1 } do
+  validates :category_id
+  validates :status_id
+  validates :burden_id
+  validates :area_id
+  validates :day_id
+  end
 
   validates_inclusion_of :price, in: 300..9_999_999
-  end
 
 end
 
