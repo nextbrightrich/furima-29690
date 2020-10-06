82b86513a3a9ba8cc115d3ad98ba1a486ef8a401
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-10-06 19:35:50 +0900

    一覧画面表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index]
 
   def index
-    @items = Item.includes(:user).order('created_at DESC')
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
