b18940721ae0a51e6a1d96c698faa8daf5c230d2
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-10-08 17:09:47 +0900

    items_controllerのupdate編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,9 +28,12 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to item_path
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+    redirect_to item_path(@item)
+    else
+      render:edit
+    end  
   end
 
 
