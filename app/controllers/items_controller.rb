00489ab7481ef2eddc1fb64ae1542e352818104d
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-10-08 19:19:05 +0900

    るuser_idとcurrent_userのidが一致しない場合にトップページに遷移するようにコントローラー側で制御

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,7 +42,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in?
+    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id 
   end
 
   def set_item
