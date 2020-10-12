61ea1c42a8b4e43993ba8033c243b0ba689a87d9
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-10-12 19:28:04 +0900

    card.js作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,8 +8,13 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
     @item = Item.find(params[:item_id])
-    if @order.varid?
-      @order.save
+    Payjp.api_key = "sk_test_1d1d27726a2f9c27fde5c475"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+    if @order.save
       return redirect_to root_path
     else
       render 'index'
@@ -19,7 +24,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order) .permit(:user, :item)
+    params.require(:order).permit(:price).merge(token: params[:token])
   end
 
 end
