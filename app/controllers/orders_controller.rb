ece81e08d96dc6b6eeb9e46756efb615155e7e9f
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-10-14 21:39:09 +0900

    クレジット機能と、購入機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,30 +1,48 @@
 class OrdersController < ApplicationController
+  before_action :move_to_index, only: [:index]
 
   def index 
     @order = Order.new
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
+  end
+
+  def new
   end
   
   def create
-    @order = Order.new(order_params)
+    @order_address = OrderAddress.new(order_params)
     @item = Item.find(params[:item_id])
-    Payjp.api_key = "sk_test_1d1d27726a2f9c27fde5c475"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
-    Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
-      card: order_params[:token],    # カードトークン
-      currency: 'jpy'                 # 通貨の種類（日本円）
-    )
-    if @order.save
+    if @order_address.valid?
+      pay_item
+      @order_address.save
       return redirect_to root_path
     else
-      render 'index'
+      render action::index
     end
+
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:price).merge(token: params[:token])
+    params.require(:order_address).permit(:postalcode, :area_id, :municipality , :housenumber, :building, :telephone).merge(token: params[:token], user_id: current_user.id,item_id:params[:item_id])
   end
+
+  def pay_item
+  Payjp.api_key = "sk_test_1d1d27726a2f9c27fde5c475"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+  Payjp::Charge.create(
+    amount:@item.price,  # 商品の値段
+    card: order_params[:token],    # カードトークン
+    currency: 'jpy'                 # 通貨の種類（日本円）
+  )
+end
+
+def move_to_index
+  @item = Item.find(params[:item_id])
+  if current_user.id == @item.user_id
+  redirect_to root_path 
+  end
+end
 
 end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -8,15 +8,21 @@ const pay = () => {
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("order[number]"),
-      cvc: formData.get("order[cvc]"),
-      exp_month: formData.get("order[exp_month]"),
-      exp_year: `20${formData.get("order[exp_year]")}`,
+      
+      number: formData.get("order_address[number]"),
+      cvc: formData.get("order_address[cvc]"),
+      exp_month: formData.get("order_address[exp_month]"),
+      exp_year: `20${formData.get("order_address[exp_year]")}`,
     };
 
+    console.log(card)
+
+
     Payjp.createToken(card, (status, response) => {
+      
       if (status == 200) {
         const token = response.id;
+        console.log(token)
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token' type="hidden"> `;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
