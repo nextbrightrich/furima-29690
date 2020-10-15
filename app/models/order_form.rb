3d9111a5c65bf3568329bccb509ca4e5b31445ec
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-10-15 17:11:28 +0900

    不要な記述の削除

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,10 +1,2 @@
-class OrderAddress
-
-  include ActiveModel::Model
-  attr_accessor :firstname, :familyname, :postal_code, :area_id :municipality , :city, :house_number, :building_name,:telephone
-  # ここにバリデーションの処理を書く
-
-  def save
-  # 各テーブルにデータを保存する処理を書く
-  end
+class OrderForm
 end
