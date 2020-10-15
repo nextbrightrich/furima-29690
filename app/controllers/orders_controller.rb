0f5bb693734659e398586a9fde5264e69f0958b3
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-10-15 18:02:04 +0900

    訂正③

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
   def index
-    return redirect_to root_path if current_user.id == set_item.user_id || @item.order.nil?
+    return redirect_to root_path if current_user.id == set_item.user_id || @item.order != nil
     @order_address = OrderAddress.new
   end
 
