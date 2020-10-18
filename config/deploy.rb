0ec447b2b599c1a95680e43cd99adf92af9d4d2e
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-10-18 10:57:07 +0900

    ''訂正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'furimaー29690'
 set :repo_url, "git@example.com:me/nextbrightrich/furima-29690"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
-set :rbenv_ruby, ’2.6.5’
+set :rbenv_ruby, '2.6.5'
 set :ssh_options, auth_methods: ['publickey'],
     keys: ['~/.ssh/nextbrightrich726.pem'] 
 
