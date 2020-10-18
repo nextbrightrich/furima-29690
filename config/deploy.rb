ff6ccf9f91e9bc2e25ea7e9d8aacc8f97c59e6b8
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-10-18 10:58:58 +0900

    -訂正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
-set :application, 'furimaー29690'
+set :application, 'furima-29690'
 set :repo_url, "git@example.com:me/nextbrightrich/furima-29690"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
