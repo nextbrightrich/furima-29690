8a94d241cba1efe07cedbd6bbc41dd39762580c6
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-10-19 12:43:24 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.14.1"
 
 set :application, 'furima-29690'
-set :repo_url, 'git@example.com:nextbrightrich/furima-29690.git'
+set :repo_url,  'git@github.com:nextbrightrich/furima-29690.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
