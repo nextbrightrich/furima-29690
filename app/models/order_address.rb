c72b1be3310484da74fc1ece20d9da8d602fb1ba
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-10-21 19:25:22 +0900

    エラーコード日本語

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,14 +3,14 @@ class OrderAddress
   attr_accessor :user_id, :item_id, :postalcode, :area_id, :municipality, :housenumber, :building, :telephone, :token
 
   with_options presence: true do
-    validates :postalcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :postalcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :municipality
     validates :housenumber
     validates :telephone, format: { with: /\A0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1})[-)]?\d{4}\z|\A0[5789]0[-]?\d{4}[-]?\d{4}\z/ }
     validates :token
   end
 
-  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :area_id, numericality: { other_than: 1 }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
